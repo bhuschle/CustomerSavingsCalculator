e895630d0f78ca1c0f8ec196f13b3b35dbe0e6b5
bhuschle <72930166+bhuschle@users.noreply.github.com>
2020-11-22 16:44:59 -0500

    update
    changed to reflect update on webpage

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// get service selected value
-let selected = document.getElementById('service').value;
-
 // get plan selected value
 let plan = document.getElementById('tmo-plan').value;
 
@@ -9,15 +6,7 @@ let line = document.getElementById('lines').value;
 let ls = document.getElementById('line-select');
 
 // get competitors plan select
-let comp = document.getElementById('comp');
-
-// Get Provider elements
-// AT&T
-let att = document.getElementById('att');
-let att_plan = document.getElementById('att-plan').value;
-// Verizon
-let vzw = document.getElementById('verizon');
-let vzw_plan = document.getElementById('vz-plan').value;
+let comp = document.getElementById('comp-plans').value;
 
 // get TMO plan box elements
 let planb = document.getElementById('plan-box');
@@ -32,12 +21,6 @@ let pp = document.getElementById('plan-price');
 let opp = document.getElementById('old-plan-price');
 let ps = document.getElementById('plan-savings');
 let pb = document.getElementById('plan-benefits');
-
-document.getElementById('tmo-plan').addEventListener("onchange", e => {
-    e.preventDefault();
-    location.reload();
-});
-
 
 // Set values for different plans
 
@@ -114,7 +97,7 @@ let ess_lines = [60, 90, 105, 120, 135, 150];
 let m_lines = [70, 120, 140, 160, 180, 200, 220, 240, 270, 300, 330, 360];
 
 // Magenta Plus Plan
-let mplus_lines = [85, 140, 170, 200, 230, 260, 290, 320, 360, 400, 440, 480];
+let mp_lines = [85, 140, 170, 200, 230, 260, 290, 320, 360, 400, 440, 480];
 
 // Magenta Plus Unlimited 55 Plan
 // 2 lines only
@@ -133,174 +116,171 @@ let mil_fr_lines = [55, 80, 90, 100, 110, 120, 140, 170, 195, 220, 245, 270];
 // 2 or more lines only
 let milp_frp_lines = [-1, 100, 120, 140, 160, 180, 210, 240, 275, 310, 345, 380];
 
+function calculateCard() {
+    plan = document.getElementById('tmo-plan').value;
+    ap = document.getElementById('autopay-select');
+    tap = document.getElementById('tmo-autopay-select');
+    line = document.getElementById('lines').value;
+    comp = document.getElementById('comp-plans').value;
 
-// Make plan values invisible until SP is chosen
-att.style.visibility = 'hidden';
-att.style.display = 'none';
-vzw.style.visibility = 'hidden';
-vzw.style.display = 'none';
-ls.style.visibility = 'hidden';
-ls.style.display = 'none';
-comp.style.visibility = 'hidden';
-comp.style.display = 'none';
+    // Previous Carrier Plan Cost
+    let previous_price = 0;
+    let selected = "";
 
-// Make TMO plan boxes invisible until TMO plan is chosen
-pb.style.visibility = 'hidden';
-pb.style.display = 'none';
-
-// Make line number selection visible if SP is selected
-if (plan != "") {
-    ls.style.visibility = 'visible';
-    ls.style.display = 'block';
-}
-
-// Make competitors plans visible if TMO plan is selected
-if (line != "") {
-    comp.style.visibility = 'visible';
-    comp.style.display = 'block';
-}
-
-// Make plan values visible for selected SP
-if (selected === 'ATT' && line != "") {
-    att.style.visibility = 'visible';
-    att.style.display = 'block';
-} else if (selected === 'VZW' && line != "") {
-    vzw.style.visibility = 'visible';
-    vzw.style.display = 'block';
-} else {
-    att.style.visibility = 'hidden';
-    att.style.display = 'none';
-    vzw.style.visibility = 'hidden';
-    vzw.style.display = 'none';
-}
-
-if (plan != "") {
-    planb.style.visibility = 'visible';
-    planb.style.display = 'block';
-} else {
-    planb.style.visibility = 'hidden';
-    planb.style.display = 'none';
-}
-
-// Previous Carrier Plan Cost
-let previous_price = 0;
-
-if (selected === 'ATT') {
-    if (att_plan === 'us') {
+    if (comp === 'us') {
         previous_price = us_lines[line - 1];
-    } else if (att_plan === 'uex') {
+        selected = "AT&T";
+    } else if (comp === 'uex') {
         previous_price = uex_lines[line - 1];
-    } else if (att_plan === 'uel') {
+        selected = "AT&T";
+    } else if (comp === 'uel') {
         previous_price = ue_lines[line - 1];
-    }
-    if (ap.checked === false) { previous_price += (10 * line); }
-} else if (selected === 'VZW') {
-    if (vzw_plan === 'su') {
+        selected = "AT&T";
+    } else if (comp === 'su') {
         previous_price = su_lines[line - 1];
-    } else if (vzw_plan === 'pmu') {
+        selected = "Verizon";
+    } else if (comp === 'pmu') {
         previous_price = pmu_lines[line - 1];
-    } else if (vzw_plan === 'dmu') {
+        selected = "Verizon";
+    } else if (comp === 'dmu') {
         previous_price = dmu_lines[line - 1];
-    } else if (vzw_plan === 'gmu') {
+        selected = "Verizon";
+    } else if (comp === 'gmu') {
         previous_price = gmu_lines[line - 1];
-    } else if (vzw_plan === 'jk') {
+        selected = "Verizon";
+    } else if (comp === 'jk') {
         previous_price = jk_lines[line - 1];
+        selected = "Verizon";
     }
     if (ap.checked === false) { previous_price += (10 * line); }
+
+    // T-Mobile Plan Price
+    let plan_price = 0;
+    // Total savings by switching to T-Mobile
+    let plan_savings = 0;
+
+
+
+    if (plan === 'ff') {
+        if (line > 2) {
+            pn.innerHTML = '<h1>Plan has a 2 line maximum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else if (line < 2 && selected === 'VZW' && vzw_plan === 'jk') {
+            pn.innerHTML = '<h1>VZW Plan has a 2 line minimum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else {
+            plan_price = ff_lines[line - 1];
+            if (tap.checked === false) { plan_price += (5 * line); }
+            plan_savings = previous_price - plan_price;
+            pn.innerHTML = `<h1>Magenta 55</h1>`;
+            opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
+            pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
+            ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
+        }
+    } else if (plan === 'ffe') {
+        if (line > 2) {
+            pn.innerHTML = '<h1>Plan has a 2 line maximum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else if (line < 2 && selected === 'VZW' && vzw_plan === 'jk') {
+            pn.innerHTML = '<h1>VZW Plan has a 2 line minimum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else {
+            plan_price = ffe_lines[line - 1];
+            if (tap.checked === false) { plan_price += (5 * line); }
+            plan_savings = previous_price - plan_price;
+            pn.innerHTML = `<h1>Magenta 55 Essentials</h1>`;
+            opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
+            pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
+            ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
+        }
+    } else if (plan === 'ffp') {
+        if (line > 2) {
+            pn.innerHTML = '<h1>Plan has a 2 line maximum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else if (line < 2 && selected === 'VZW' && vzw_plan === 'jk') {
+            pn.innerHTML = '<h1>VZW Plan has a 2 line minimum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else {
+            plan_price = ffp_lines[line - 1];
+            if (tap.checked === false) { plan_price += (5 * line); }
+            plan_savings = previous_price - plan_price;
+            pn.innerHTML = `<h1>Magenta 55+</h1>`;
+            opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
+            pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
+            ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
+        }
+    } else if (plan === 'ess') {
+        if (line > 6) {
+            pn.innerHTML = '<h1>Plan has a 6 line maximum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else if (line < 2 && selected === 'VZW' && vzw_plan === 'jk') {
+            pn.innerHTML = '<h1>VZW Plan has a 2 line minimum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else {
+            plan_price = ess_lines[line - 1];
+            if (tap.checked === false) { plan_price += (5 * line); }
+            plan_savings = previous_price - plan_price;
+            pn.innerHTML = `<h1>Magenta Essentials</h1>`;
+            opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
+            pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
+            ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
+        }
+    } else if (plan === 'milfrp') {
+        if (line < 2) {
+            pn.innerHTML = '<h1>Plan has a 2 line minimum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else if (line < 2 && selected === 'VZW' && vzw_plan === 'jk') {
+            pn.innerHTML = '<h1>VZW Plan has a 2 line minimum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else {
+            plan_price = milp_frp_lines[line - 1];
+            if (tap.checked === false) { plan_price += (5 * line); }
+            plan_savings = previous_price - plan_price;
+            pn.innerHTML = `<h1>Magenta Military / First Responder Plus</h1>`;
+            opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
+            pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
+            ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
+        }
+    } else if (plan === 'milfr') {
+        if (line < 2 && selected === 'VZW' && vzw_plan === 'jk') {
+            pn.innerHTML = '<h1>VZW Plan has a 2 line minimum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else {
+            plan_price = mil_fr_lines[line - 1];
+            if (tap.checked === false) { plan_price += (5 * line); }
+            plan_savings = previous_price - plan_price;
+            pn.innerHTML = `<h1>Magenta Military / First Responder</h1>`;
+            opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
+            pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
+            ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
+        }
+    } else if (plan === 'm') {
+        if (line < 2 && selected === 'VZW' && vzw_plan === 'jk') {
+            pn.innerHTML = '<h1>VZW Plan has a 2 line minimum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else {
+            plan_price = m_lines[line - 1];
+            if (tap.checked === false) { plan_price += (5 * line); }
+            plan_savings = previous_price - plan_price;
+            pn.innerHTML = '<h1>Magenta</h1>';
+            opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
+            pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
+            ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
+        }
+    } else if (plan === 'mp') {
+        if (line < 2 && selected === 'VZW' && vzw_plan === 'jk') {
+            pn.innerHTML = '<h1>VZW Plan has a 2 line minimum<h1>';
+            pp.innerHTML = `<h1>${line} lines selected<h1>`;
+        } else {
+            plan_price = mp_lines[line - 1];
+            if (tap.checked === false) { plan_price += (5 * line); }
+            plan_savings = previous_price - plan_price;
+            pn.innerHTML = `<h1>Magenta +</h1>`;
+            opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
+            pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
+            ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
+        }
+    }
 }
 
-// T-Mobile Plan Price
-let plan_price = 0;
-// Total savings by switching to T-Mobile
-let plan_savings = 0;
-
-if (plan === 'ff') {
-    if (line > 2) {
-        pn.innerHTML = '<h1>Plan has a 2 line maximum<h1>';
-        pp.innerHTML = `<h1>${line} lines selected<h1>`;
-    } else {
-        plan_price = ff_lines[line - 1];
-        plan_savings = previous_price - plan_price;
-        pn.innerHTML = `<h1>Magenta 55</h1>`;
-        if (tap.checked === false) { plan_price += (5 * line); }
-        opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
-        pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
-        ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
-    }
-} else if (plan === 'ffe') {
-    if (line > 2) {
-        pn.innerHTML = '<h1>Plan has a 2 line maximum<h1>';
-        pp.innerHTML = `<h1>${line} lines selected<h1>`;
-    } else {
-        plan_price = ffe_lines[line - 1];
-        plan_savings = previous_price - plan_price;
-        pn.innerHTML = `<h1>Magenta 55 Essentials</h1>`;
-        if (tap.checked === false) { plan_price += (5 * line); }
-        opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
-        pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
-        ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
-    }
-} else if (plan === 'ffp') {
-    if (line > 2) {
-        pn.innerHTML = '<h1>Plan has a 2 line maximum<h1>';
-        pp.innerHTML = `<h1>${line} lines selected<h1>`;
-    } else {
-        plan_price = ffp_lines[line - 1];
-        plan_savings = previous_price - plan_price;
-        pn.innerHTML = `<h1>Magenta 55+</h1>`;
-        if (tap.checked === false) { plan_price += (5 * line); }
-        opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
-        pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
-        ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
-    }
-} else if (plan === 'ess') {
-    if (line > 6) {
-        pn.innerHTML = '<h1>Plan has a 6 line maximum<h1>';
-        pp.innerHTML = `<h1>${line} lines selected<h1>`;
-    } else {
-        plan_price = ess_lines[line - 1];
-        plan_savings = previous_price - plan_price;
-        pn.innerHTML = `<h1>Magenta Essentials</h1>`;
-        if (tap.checked === false) { plan_price += (5 * line); }
-        opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
-        pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
-        ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
-    }
-} else if (plan === 'milfrp') {
-    if (line < 2) {
-        pn.innerHTML = '<h1>Plan has a 2 line minimum<h1>';
-        pp.innerHTML = `<h1>${line} lines selected<h1>`;
-    } else {
-        plan_price = milp_frp_lines[line - 1];
-        plan_savings = previous_price - plan_price;
-        pn.innerHTML = `<h1>Magenta Military / First Responder Plus</h1>`;
-        if (tap.checked === false) { plan_price += (5 * line); }
-        opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
-        pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
-        ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
-    }
-} else if (plan === 'milfr') {
-    plan_price = mil_fr_lines[line - 1];
-    plan_savings = previous_price - plan_price;
-    pn.innerHTML = `<h1>Magenta Military / First Responder</h1>`;
-    if (tap.checked === false) { plan_price += (5 * line); }
-    opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
-    pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
-    ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
-} else if (plan === 'm') {
-    plan_price = m_lines[line - 1];
-    plan_savings = previous_price - plan_price;
-    pn.innerHTML = '<h1>Magenta</h1>';
-    if (tap.checked === false) { plan_price += (5 * line); }
-    opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
-    pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
-    ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
-} else if (plan === 'mp') {
-    plan_price = mp_lines[line - 1];
-    plan_savings = previous_price - plan_price;
-    pn.innerHTML = `<h1>Magenta +</h1>`;
-    if (tap.checked === false) { plan_price += (5 * line); }
-    opp.innerHTML = `<p>${selected} Cost : <strong>${previous_price}</strong> per month</p>`;
-    pp.innerHTML = `<p>T-Mobile Cost : <strong>${plan_price}</strong> per month</p>`;
-    ps.innerHTML = `<p>Savings : <strong>${plan_savings}</strong> per month</p>`;
-}
+document.getElementById('submit').addEventListener('click', calculateCard);
